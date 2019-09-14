eb01a1f4d7f61cbbdff2c8670ec1896c364ab658
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2019-09-13 22:54:38 -0300

    Add how to find longest binary gap

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -34,3 +34,34 @@ data = """
 
 let otherModel: MyOtherModel = decode(modelType: MyOtherModel.self)
 print(otherModel.otherName)
+
+//---------------------------------------------------------
+
+public func solution(_ N : Int) -> Int {
+    let binaryArray = convertToBinary(N)
+    var count: Int = 0
+    var max: Int = 0
+    
+    for index in 0...(binaryArray.count - 1) {
+        if binaryArray[index] == "1" {
+            if count > 0 {
+                max = max > count ? max : count
+            }
+            count = 0
+        }
+        
+        if binaryArray[index] == "0" {
+            count = count + 1
+        }
+    }
+    return max
+}
+
+func convertToBinary(_ N: Int) -> [Character] {
+    let binary = String(N, radix: 2)
+    let array = Array(binary)
+    print(array)
+    return array
+}
+
+print(solution(110101))
